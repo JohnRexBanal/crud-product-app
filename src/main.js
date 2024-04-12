04d3c9e3dabbe35f962aa227b9e218a9566cd808
JohnRexBanal <banaljohnrex99@gmail.com>
2024-04-12 20:21:39 +0800

    Updated the main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';  
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';  
+import ProductList from './components/ProductList.vue';
+import AddProduct from './components/AddProduct.vue';
+import EditProduct from './components/EditProduct.vue';
 
-createApp(App).use(router).mount('#app')
+const routes = [
+  { path: '/', component: ProductList },
+  { path: '/add', component: AddProduct },
+  { path: '/edit/:id', component: EditProduct, props: true }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
